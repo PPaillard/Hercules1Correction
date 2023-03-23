98a68b5e7ede2f7158705b577763a386b859b6b8
PierreP <53476143+RoYaLSleT@users.noreply.github.com>
2023-03-23 15:30:50 +0100

    ending correction

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
 // First Labour : Heracles vs Nemean Lion
+const Fighter = require("./src/Fighter");
+
+const heraclette = new Fighter("🧔 Heracles", 20, 6);
+const simba = new Fighter("🦁 Nemean Lion", 11, 13);
+
 // use your Figher class here
+console.log(`${heraclette.name} have ${heraclette.life} life points 💙`);
+console.log(`${simba.name} have ${simba.life} life points 💙`);
+
+let round = 1;
+
+do {
+  console.log(`🕛 Round #${round}`);
+  heraclette.fight(simba);
+  if (simba.isAlive()) simba.fight(heraclette);
+  round++;
+} while (heraclette.isAlive() && simba.isAlive());
+
+// ternaire qui renvoi un objet avec le bon looser / winner. On l'assigne en déstructuration à la sortie
+const { winner, looser } = heraclette.isAlive()
+  ? { looser: simba, winner: heraclette }
+  : { looser: heraclette, winner: simba };
+// grâce à la méthode plus haut, on peut n'écrit qu'une seul fois le code si dessous.
+console.log(`💀 ${looser.name} is dead`);
+console.log(`🏆 ${winner.name} wins (${winner.life} 💙)`);
+
+/*
+if (heraclette.life === 0) {
+  console.log(`💀 ${heraclette.name} is dead`);
+  console.log(`🏆 ${simba.name} wins (${simba.life} 💙)`);
+} else {
+  console.log(`💀 ${simba.name} is dead`);
+  console.log(`🏆 ${heraclette.name} wins (${heraclette.life} 💙)`);
+}
+*/
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,35 @@
 /* Fighter class definition */
+const MAX_LIFE = 100;
+
+class Fighter {
+  constructor(name, strength, dexterity) {
+    this.name = name;
+    this.strength = strength;
+    this.dexterity = dexterity;
+    this.life = MAX_LIFE;
+  }
+
+  fight(enemy) {
+    // on prend un nombre random entre 1 et la force de l'enemi
+    const randomDamage = Math.floor(Math.random() * this.strength) + 1;
+
+    // on calcul les dommages final
+    /*let finalDamage = randomDamage - enemy.dexterity;
+    if(finalDamage< 0) finalDamage = 0*/
+    const finalDamage = Math.max(randomDamage - enemy.dexterity, 0);
+
+    // si les dommages sont inférieurs à 0, on met à 0
+    enemy.life = Math.max(enemy.life - finalDamage, 0);
+    console.log(
+      `${this.name} attack ${enemy.name} doing ${finalDamage} damage.${enemy.name} have ${enemy.life} life points 💙`
+    );
+  }
+
+  isAlive() {
+    /*if(this.life>0) return true;
+    else return false;*/
+    return this.life > 0;
+  }
+}
+
+module.exports = Fighter;
